99b228e3057b99d7499683a33c417e7c594f48f6
Johannes Goslar <johannes.goslar@dkd.de>
2015-04-10 13:42:14 +0200

    [BUGFIX] 6.2 compatibility #60900

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,7 +13,7 @@ tx_solrmlt_ClassLoader::loadClasses();
 	// adding the More Like This plugin
 t3lib_extMgm::addPItoST43(
 	'solr',
-	'pi_mlt/class.tx_solr_pi_mlt.php',
+	'../solrmlt/pi_mlt/class.tx_solr_pi_mlt.php',
 	'_pi_mlt',
 	'list_type',
 	TRUE
