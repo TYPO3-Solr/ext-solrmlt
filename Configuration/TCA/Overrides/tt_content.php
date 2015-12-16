2a8a1921ac79bf03489c248423df4ffeab2569ef
Ingo Renner <ingo@typo3.org>
2015-12-15 23:20:46 -0800

    Move plugin registration to tt_content TCA overrides

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,19 @@
+<?php
+
+// adding the More Like This plugin
+$pluginCode = 'solr_pi_mlt';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    array(
+        'LLL:EXT:solrmlt/Resources/Private/Language/locallang_db.xml:tt_content.list_type_pi_mlt',
+        $pluginCode
+    ),
+    'list_type'
+);
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginCode] = 'layout,select_key,pages,recursive';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginCode] = 'pi_flexform';
+
+// add flexform to pi_mlt
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginCode,
+    'FILE:EXT:solrmlt/Configuration/FlexForms/MoreLikeThis.xml'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -2,22 +2,3 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
-
-   # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- #
-
-// adding the More Like This plugin
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-    array(
-        'LLL:EXT:solrmlt/Resources/Private/Language/locallang_db.xml:tt_content.list_type_pi_mlt',
-        'solr_pi_mlt'
-    ),
-    'list_type'
-);
-$TCA['tt_content']['types']['list']['subtypes_excludelist']['solr_pi_mlt'] = 'layout,select_key,pages,recursive';
-$TCA['tt_content']['types']['list']['subtypes_addlist']['solr_pi_mlt'] = 'pi_flexform';
-
-// add flexform to pi_mlt
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'solr_pi_mlt',
-    'FILE:EXT:solrmlt/Configuration/FlexForms/MoreLikeThis.xml'
-);
