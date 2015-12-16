9c33923a81cfb400acfc7b8c9820c616c4f2a097
Ingo Renner <ingo@typo3.org>
2015-12-15 23:00:36 -0800

    organize ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,33 +1,22 @@
 <?php
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "solrmlt".
- *
- * Auto generated 10-04-2015 10:12
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Apache Solr for TYPO3 - More Like This',
     'description' => 'More Like This',
+    'version' => '1.1.0-dev',
+    'state' => 'stable',
     'category' => 'plugin',
     'author' => 'Ingo Renner',
     'author_email' => 'ingo@typo3.org',
+    'author_company' => 'dkd Internet Service GmbH',
     'module' => '',
-    'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
-    'author_company' => 'dkd Internet Service GmbH',
-    'version' => '1.1.0-dev',
     'constraints' => array(
         'depends' => array(
             'solr' => '3.1.0-',
-            'typo3' => '6.2.0-7.9.9',
+            'typo3' => '6.2.0-7.99.99',
         ),
         'conflicts' => array(
         ),
